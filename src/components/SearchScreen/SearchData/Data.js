eb397f859bed70cdc8fe103c433a5ce05463fcc2
Harshit Roy <101564851+harshit-roy@users.noreply.github.com>
2023-07-15 03:00:14 +0530

    working on search page

--- src/components/SearchScreen/SearchData/Data.js
+++ src/components/SearchScreen/SearchData/Data.js
@@ -5,8 +5,15 @@ const Data = ({ data }) => {
   return (
     <div className="col-md-12">
       <Link to={data.displayLink}>
-      {data.displayLink}
+      <h7>{data.displayLink}{" "}
+      <i className="fa fa-angle-down"></i></h7>
       </Link>
+      <Link to={data.formattedUrl}>
+      <h4>
+      {data.title}
+      </h4>
+      </Link>
+      <p>{data.snippet}</p>
     </div>
   );
 };
