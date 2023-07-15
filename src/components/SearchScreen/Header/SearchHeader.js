d09207948ee9a8c6557418bfd646e241114cff72
Harshit Roy <101564851+harshit-roy@users.noreply.github.com>
2023-07-15 22:53:17 +0530

    working on search page

--- src/components/SearchScreen/Header/SearchHeader.js
+++ src/components/SearchScreen/Header/SearchHeader.js
@@ -7,7 +7,10 @@ const SearchHeader = ({ searchTerm }) => {
     setSearchText(searchTerm);
   }, [searchTerm]);
   return (
-    <div className="col-md-12 d-flex align-items-center" style={{paddingTop:"1.5%",paddingBottom:".5%"}}>
+    <div
+      className="col-md-12 d-flex align-items-center"
+      style={{ paddingTop: "1.5%", paddingBottom: ".5%" }}
+    >
       <Link to={"/"} className="ms-5 mx-3">
         <img
           src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png"
@@ -15,25 +18,39 @@ const SearchHeader = ({ searchTerm }) => {
           height={30}
         />
       </Link>
-      <div className="col-md-5 d-flex align-items-center justify-content-between border border-secondary pe-3 rounded">
+      <div
+        className=" px-3 col-md-5 d-flex align-items-center justify-content-between border border-secondary pe-3"
+        style={{
+          borderBottomRightRadius: "50px",
+          borderBottomLeftRadius: "50px",
+          borderTopRightRadius: "50px",
+          borderTopLeftRadius: "50px",
+        }}
+      >
         <form style={{ width: "85%" }}>
           <input
             type="text"
             value={searchText}
             onChange={(e) => setSearchText(e.target.value)}
-            className="form-control border-0 me-2"
+            className="form-control border-0 me-2 px-1"
             style={{ outline: "none", boxShadow: "none" }}
           />
         </form>
         {searchText ? (
           <i className="fa fa-times mx-1" style={{ cursor: "pointer" }}></i>
         ) : null}
-        <i className="fa-solid fa-microphone mx-1" style={{ cursor: "pointer" }}></i>
-        <i className="fa-solid fa-magnifying-glass mx-1" style={{ cursor: "pointer" }}></i> 
+        <i
+          className="fa-solid fa-microphone mx-1"
+          style={{ cursor: "pointer" }}
+        ></i>
+        <i
+          className="fa-solid fa-magnifying-glass mx-1"
+          style={{ cursor: "pointer" }}
+        ></i>
       </div>
       <ul className="nav ms-auto me-5">
         <li className="nav-items">
-          <a href="/" alt="apps" className="nav-link" >
+          <a href="/" alt="apps" className="nav-link">
             <i className="fa fa-th" style={{ color: "black" }}></i>
           </a>
         </li>
