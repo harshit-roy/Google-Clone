11d232fb94c44330de55c9a14bf471110876d112
Harshit Roy <101564851+harshit-roy@users.noreply.github.com>
2023-07-18 17:57:28 +0530

    working on search page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,49 @@
 import logo from "./logo.svg";
-import {  Routes, Route, useNavigate, Navigate } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  useNavigate,
+  Navigate,
+  Redirect,
+} from "react-router-dom";
 import React, { useState } from "react";
 import "./App.css";
 import Home from "./components/HomeScreen/Home";
 import SearchScreen from "./components/SearchScreen/SearchScreen";
-import {searchData} from './Api/GoogleSearch';
+import { searchData } from "./Api/GoogleSearch";
 
 const App = () => {
-    const Navigate = useNavigate();
-  const [searchTerm, setSearchTerm] = useState('');
-  const [googleData,setGoogleData] = useState({});
+  const Navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState("");
+  const [googleData, setGoogleData] = useState({});
   const setSearch = async (term) => {
     setSearchTerm(term);
     const data = await searchData(term);
     setGoogleData(data);
-    Navigate('/search');
+    Navigate("/search");
   };
 
   return (
-    
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home setSearch={setSearch}/>} />
-          <Route
+    <div className="App">
+      <Routes>
+        <Route 
+        exact 
+        path="/" 
+        element={<Home setSearch={setSearch} />} />
+        {
+          searchTerm !== "" ? 
+          (<Route
             path="/search"
-            element={<SearchScreen searchTerm={searchTerm} 
-            googleData={googleData}
-            />}
+            element={
+              <SearchScreen searchTerm={searchTerm} googleData={googleData} />
+            }
           />
-        </Routes>
-      </div>
+        ) : null
+        }
+        <Route path="/" />
+      </Routes>
+      
+    </div>
   );
 };
 
